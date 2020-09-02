e217e08336b5d4d743856d09042f84b01241ddff
Shaxadhere <shaxad.here@gmail.com>
2020-09-02 15:30:32 +0500

    charts

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ include_once('rapid.php');
 
 
 
-$chartUrl = generateChart(400, 400, array(20,30,30,20), array("PHP", "C", "Java", "Swift"), array('442C96','7C1E5D','56962C', '7C1E8A'), 1);
+$chartUrl = generateChart(400, 400, array(20,30,30,20), array("PHP", "C", "Java", "Typescript"), array('442C96','7C1E5D','56962C', '7C1E8A'), 1);
 ?>
 <!DOCTYPE html>
 <html lang="en">
